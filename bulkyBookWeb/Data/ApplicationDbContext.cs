c176ebb146e4fa6a8eba09b49ece5003d1d8c7b7
hardeymolar <abdulmalikgbolahan95@gmail.com>
2022-08-13 20:13:36 +0100

    Add project files.

---
+++ bulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using bulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bulkyBookWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
---
+++ bulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace bulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set;}
+        [Required]
+        public String Name { get; set; }
+
+        [DisplayName ("Display Order") ]
+        [Range(1,100,ErrorMessage ="Display oreder must be between 1 to 100")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTine { get; set; } = DateTime.Now;
+
+    }
+}
